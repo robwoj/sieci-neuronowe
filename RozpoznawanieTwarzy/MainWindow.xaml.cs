b4e388cec6b31786c245b2b49554ad45771e9ba9
Robert Wojciechowski <robwoj89@mat.umk.pl>
2011-11-28 22:20:45 +0100

    1) Wstępna implementacja algorytmu Oja
    2) Wstępna implementacja algorytmu redukcji wymiarów przez wyznaczanie składowych głównych

--- RozpoznawanieTwarzy/MainWindow.xaml.cs
+++ RozpoznawanieTwarzy/MainWindow.xaml.cs
@@ -23,12 +23,18 @@ namespace RozpoznawanieTwarzy
     public partial class MainWindow : Window
     {
         private FolderBrowserDialog openDialog;
-        private List<PerceptronLib.Vector> examples;
+        private List<LearningExample> examples;
+        private int iterations;
+        private int examplesWidth;
+        private int examplesHeight;
         public MainWindow()
         {
             InitializeComponent();
             openDialog = new FolderBrowserDialog();
             openDialog.Description = "Podaj katalog z przykładami";
+
+            learnButton.IsEnabled = false;
+            iterations = 10;
             //openDialog.Filter = "Plik JPEG (*.jpg)|*.jpg";
             //openDialog.FilterIndex = 1;
             
@@ -63,19 +69,47 @@ namespace RozpoznawanieTwarzy
                 }
 
                 examples = createLearningExamples(files);
+
+                if (examples != null)
+                    learnButton.IsEnabled = true;
             }
         }
 
-        private List<PerceptronLib.Vector> createLearningExamples(List<FileInfo> files)
+        private void saveImages(List<Perceptron> vectors, int width, int height)
         {
-            List<PerceptronLib.Vector> examples = new List<PerceptronLib.Vector>();
+            for (int k = 0; k < vectors.Count; k++ )
+            {
+                Perceptron p = vectors[k];
+                Bitmap img = new Bitmap(examplesWidth, examplesHeight);
+
+                for (int i = 0; i < width; i++)
+                {
+                    for (int j = 0; j < height; j++)
+                    {
+                        int index = i * height + j;
+                        byte color = (byte)(p.Weights[index] * 256.0F);
+                        System.Drawing.Color c = System.Drawing.Color.FromArgb(255, color, color, color);
+                        img.SetPixel(i, j, c);
+                    }
+                }
+
+                img.Save("output" + (k + 1) + ".jpg");
+            }
+        }
+
+        private List<LearningExample> createLearningExamples(List<FileInfo> files)
+        {
+            List<LearningExample> examples = new List<LearningExample>();
 
             foreach (FileInfo f in files)
             {
                 Bitmap bitmap = (Bitmap)Bitmap.FromFile(f.FullName);
+                examplesWidth = bitmap.Width;
+                examplesHeight = bitmap.Height;
+
                 printLine(f.Name + ": " + bitmap.Width + "x" + bitmap.Height);
 
-                PerceptronLib.Vector v = new PerceptronLib.Vector(bitmap.Width * bitmap.Height);
+                Perceptron p = new Perceptron(bitmap.Width * bitmap.Height);
 
                 int width = bitmap.Width;
                 int height = bitmap.Height;
@@ -86,14 +120,14 @@ namespace RozpoznawanieTwarzy
                     for (int j = 0; j < height; j++)
                     {
                         int index = i * height + j;
-                        v[index] = bitmap.GetPixel(i, j).R;
-                        if (min > v[index]) min = v[index];
-                        if (max < v[index]) max = v[index];
+                        p.Weights[index] = (double)bitmap.GetPixel(i, j).R / 256.0F;
+                        //if (min > p.Weights[index]) min = p.Weights[index];
+                        //if (max < p.Weights[index]) max = p.Weights[index];
                     }
                 }
 
-                //printLine("Max = " + max + ", Min = " + min);
-                examples.Add(v);
+                printLine("Max = " + max + ", Min = " + min);
+                examples.Add(new LearningExample(p.Weights, 0));
             }
 
             return examples;
@@ -107,10 +141,93 @@ namespace RozpoznawanieTwarzy
             }
         }
 
+        private void reduction(List<LearningExample> exampleList)
+        {
+            List<LearningExample> list = exampleList;
+            int dimension = exampleList[0].Example.Dimension;
+            int m = 6;
+            List<Perceptron> principalComponents = new List<Perceptron>(m);
+            for (int i = 0; i < m; i++)
+            {
+                printLine("i = " + i);
+                //Perceptron p = new Perceptron(dimension);
+                //for (int j = 0; j < dimension; j++)
+                //{
+                //    p.Weights[j] /= (double)dimension;
+                //}
+
+                //printLine("[" + p.Weights[0] + ";" + p.Weights[1] + ";" + p.Weights[2] + "]");
+                principalComponents.Add(ojLearn(list));
+                Perceptron p = principalComponents[i];
+                printLine("[" + p.Weights[0] + ";" + p.Weights[1] + ";" + p.Weights[2] + "]");
+                List<LearningExample> nextList = new List<LearningExample>();
+                foreach (LearningExample ex in list)
+                {
+                    double val = p.Weights * p.Weights;
+                    double activation = p.Weights * ex.Example;
+                    if (activation == 0 || val == 0)
+                        printLine("act = " + activation + ", val = " + val);
+                    PerceptronLib.Vector nextExVector = new PerceptronLib.Vector(dimension);
+                    for (int j = 0; j < dimension; j++)
+                    {
+                        nextExVector[j] = ex.Example[j] - p.Weights[j] * val / activation;
+                    }
+                    LearningExample nextEx = new LearningExample(nextExVector, 0);
+                    nextList.Add(nextEx);
+                }
+                list = nextList;
+            }
+
+            saveImages(principalComponents, examplesWidth, examplesHeight);
+        }
+
+        private Perceptron ojLearn(List<LearningExample> exampleList)
+        {
+            Random r = new Random();
+            double eta = 0.5;
+            Perceptron perceptron = new Perceptron(exampleList[0].Example.Dimension);
+            for (int i = 0; i < iterations; i++)
+            {
+                LearningExample ex = exampleList[r.Next(exampleList.Count)];
+                perceptron = new Perceptron(ex.Example.Dimension);
+                double activation = perceptron.Weights * ex.Example * eta;
+                //printLine("oj-ex: [" + ex.Example[0] + ";" + ex.Example[1] + ";"
+                //    + ex.Example[2] + "]");
+                                              
+                printLine("act: " + activation);
+                for (int j = 0; j < perceptron.Dimension; j++)
+                {
+                    perceptron.Weights[j] += activation * (ex.Example[j] - 
+                        activation * perceptron.Weights[j]);
+                }
+                printLine("oj[" + i + "]: [" + perceptron.Weights[0] + ";" + perceptron.Weights[1] + ";"
+                    + perceptron.Weights[2] + "]");
+            }
+
+            return perceptron;
+        }
+
         private void printLine(string str)
         {
             konsola.Text += str + "\n";
         }
+
+        private void learnButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (examples != null)
+            {
+                reduction(examples);
+            }
+        }
+
+        private delegate void voidatstring(string str);
+
+        private voidatstring printLineDelegate;
+
+        //private void pl(string str)
+        //{
+        //    Dispatcher.Invoke();
+        //}
     }
 }
 
